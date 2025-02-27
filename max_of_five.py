b720a908a47d5d4ec9942f485ceabcfb0daebe8e
Akli Zeroual <aklizeroual2@gmail.com>
2025-02-27 10:10:55 +0100

    Add files via upload

---
+++ max_of_five.py
@@ -0,0 +1,6 @@
+numbers = []
+for i in range(5):
+    num = int(input("Type your number here: "))
+    numbers.append(num)
+biggest_number = max(numbers)
+print("The biggest typed numbers is :", biggest_number)
